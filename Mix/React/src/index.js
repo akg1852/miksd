7e8805cdbea5dab24c481e3acd591940d6572790
Adam Gray <adam.gray@noreply.com>
2018-05-03 23:21:30 +1200

    setup react, webpack, babel

---
+++ Mix/React/src/index.js
@@ -0,0 +1,4 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom'
+
+//ReactDOM.render(<div/>, document.body);
---
+++ Mix/React/webpack.config.babel.js
@@ -0,0 +1,21 @@
+﻿import path from 'path';
+
+export default {
+    mode: "production",
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        filename: 'mix.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'src'),
+                loader: 'babel-loader'
+            },
+        ]
+    }
+};
