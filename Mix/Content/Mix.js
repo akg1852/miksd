d726078c281846369d9715aa653f77297dec7013
Adam Gray <adam.gray@noreply.com>
2018-05-12 14:25:19 +1200

    make ingredient search completely fixed (rather than scrolling a bit to the top first)

--- Mix/Content/Mix.js
+++ Mix/Content/Mix.js
@@ -8,23 +8,6 @@
             ingredientSearch.style.visibility === 'visible' ? 'hidden' : 'visible';
     });
 
-    // scrolling magic
-    var header = document.getElementById('header');
-    var ingredientSearchWrapper = document.getElementById('ingredient-search-wrapper');
-    var headerBottom = document.getElementById('header-bottom');
-    window.addEventListener('scroll', function () {
-        if (window.pageYOffset > headerBottom.offsetTop - 10) {
-            ingredientSearchWrapper.style.position = 'fixed';
-            ingredientSearchWrapper.style.top = '10px';
-            ingredientSearchWrapper.style.right = (header.offsetLeft + 10) + 'px';
-        }
-        else {
-            ingredientSearchWrapper.style.position = 'absolute';
-            ingredientSearchWrapper.style.top = '';
-            ingredientSearchWrapper.style.right = '';
-        }
-    })
-
     // click away from modals
     document.body.addEventListener('click', function (e) {
         var searchResults = document.getElementById('search-results');
