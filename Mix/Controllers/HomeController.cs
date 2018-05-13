beeaec9baca2b33937c143969a4817621a922eb5
Adam Gray <adam.gray@noreply.com>
2018-05-13 13:38:22 +1200

    tidy up HomeController

--- Mix/Controllers/HomeController.cs
+++ Mix/Controllers/HomeController.cs
@@ -1,17 +1,9 @@
-﻿using Mix.Services;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Mix.Controllers
 {
     public class HomeController : BaseController
     {
-        private CocktailService cocktailService;
-
-        public HomeController()
-        {
-            cocktailService = new CocktailService();
-        }
-
         public ActionResult Index()
         {
             return View();
