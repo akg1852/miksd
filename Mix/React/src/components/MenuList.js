0c3843d91e29dff9687b4fa6c02e5b2115f6b3e8
Adam Gray <adam.gray@noreply.com>
2018-12-24 12:48:53 +1300

    improve styling of menu list

--- Mix/React/src/components/MenuList.js
+++ Mix/React/src/components/MenuList.js
@@ -20,17 +20,22 @@ class MenuList extends React.Component {
                     Create your own cocktail menus. They're saved to your browser automatically.<br />
                     When a menu is ready, use the "share" link to share a clean read-only version of it.
                 </p>
-                <ul>
-                    {this.props.menus.map(m => <li key={m.id}>
-                        <Link to={"/Menu/Edit/" + m.id}>{m.name}</Link>
-                        <span className="remove-menu-button"
-                            title="Delete menu"
-                            onClick={() => this.props.handleRemoveMenu(m.id)}
-                        >
-                            ×
-                        </span>
-                    </li>)}
-                </ul>
+                {!this.props.menus.length ?
+                    <div>Create a new menu by clicking on the plus button.</div> :
+                    <ul>
+                        {this.props.menus.map(m =>
+                            <li key={m.id} className="menus-list-item">
+                                <Link to={"/Menu/Edit/" + m.id}>{m.name}</Link>
+                                <span className="remove-menu-button"
+                                    title="Delete menu"
+                                    onClick={() => this.props.handleRemoveMenu(m.id)}
+                                >
+                                    ×
+                                </span>
+                            </li>
+                        )}
+                    </ul>
+                }
             </div>
         );
     }
