2a424d0b3c968810ce153bdba7229d4222c2c7eb
Adam Gray <adam.gray@noreply.com>
2019-01-26 18:32:08 +1300

    improve "Loading" component

--- Mix/React/src/components/Loading.js
+++ Mix/React/src/components/Loading.js
@@ -1,9 +1,42 @@
 ﻿import React from 'react';
 
 class Loading extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this. totalDots = 5;
+        this.state = {
+            dot: 0,
+            direction: -1
+        };
+
+        this.update = this.update.bind(this);
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.update, 400);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    update() {
+        let { dot, direction } = this.state;
+        if (dot == 0 || dot == this.totalDots - 1) {
+            direction = -direction;
+        }
+        dot += direction;
+        this.setState({ dot, direction });
+    }
+
     render() {
         return (
-            <div className="loading">Loading...</div>
+            <div className="loading">
+                {[...Array(this.totalDots)].map((o, i) => (
+                    <span className={'dot' + (i === this.state.dot ? ' current' : '')}>.</span>
+                ))}
+            </div>
         );
     }
 }
